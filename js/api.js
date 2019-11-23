80e76a7889627c53c0d59c11df3dd1fa2e451f3f
Zizky51017022 <you@example.com>
2019-11-23 16:25:54 +0800

    Second

--- js/api.js
+++ js/api.js
@@ -1,11 +1,11 @@
 //script untuk menangani proses responses dari proses request Api
 
 function status(response) {
-  if response.status !== 200) {
+  if (response.status !== 200) {
     console.log("Error : " + response .status);
     //Method reject () akan membuat blok catch terpanggil
 
-    return Promise.reject (new Error (response.statusText));
+    return Promise.reject (new Error(response.statusText));
   } else {
     //Mengubah suatu objek menjadi Promiseagar bisa "di-then-kan"
     return Promise.resolve (response);
@@ -17,7 +17,7 @@ function json (response) {
 
 }
 //blok kode untuk meng-handle kesalahan di blok catch
-function error (error) {
+function error(error){
   //Parameter error berasal dari Promise.reject ()
   console.log("Error : " +  error);
-} 
+}
--- js/tmdb.js
+++ js/tmdb.js
@@ -2,8 +2,32 @@ const key ='b3566690d2e7867cbf34a7d0a60d92b9'; //Api Keyy
 const endpoint_url ='https://api.themoviedb.org/3';
 
 
-function getListMovie() {
-fetch(endpoint_url + "/movie/now_playing?api_key" + key + "&language=en-US&page=1")
-then(status)
+function getListMovie(services, sectiontitle) {
+  fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
+.then(status)
+.then(json)
+.then(function (data){
 
+    var moviesHTML = " ";
+    data.results.forEach(function(movie) {
+      moviesHTML += `
+      <div class="col m3 s6">
+        <div class="card">
+          <a href="./movie.html?id=${movie.id}">
+           <div class="card-image waves-effect waves-block waves-light">
+             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+            </div>
+            </a>
+            <div class="card-content text-center">
+              <strong>${movie.title}</strong>
+            </div>
+          </div>
+        </div>
+    `;
+  });
+    document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
+})
+.catch(error);
 }
